749efcc959e98f7bbc440f426feb2c39b021185e
emmanuel <emmanuelguerreiro98@gmail.com>
2022-09-01 23:35:43 -0300

    Empezado punto 2

---
+++ TPs/TP2/procesos.c
@@ -0,0 +1,18 @@
+#include <unistd.h>
+#include <stdio.h>
+
+int main()
+{
+
+    pid_t c_id = fork();
+
+    if (c_id == 0)
+    {
+        printf("Soy el padre\n");
+    }
+    else
+    {
+        printf("pid = %ld\n", (long)c_id);
+    }
+    return 0;
+}
